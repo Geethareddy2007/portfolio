e36ded9ffc14d9c19cbb3e571f28e594ac35c7e5
Geetha reddy <k.geethareddy13@gmail.com>
2026-02-03 13:36:16 +0530

    Update portfolio.ts

--- portfolio.ts
+++ portfolio.ts
@@ -23,7 +23,7 @@ export const openSource = {
 };
 
 export const contact = {
-  image: "https://media.licdn.com/dms/image/v2/D4D03AQG0BVt37CclBg/profile-displayphoto-shrink_400_400/B4DZTqEwJkHkAg-/0/1739093903285?e=1771459200&v=beta&t=Znxrc1uEh63WDXQXBDVA8-IU2MpYRRKnq6N-u5upWCU", 
+  image: "/img/icons/common/im.jpeg", 
 };
 
 export const socialLinks: SocialLinksType = {
